6e148932cf6dd7f28fed2084a265a7d09aba6a0e
David <2596690984@qq.com>
2021-10-13 15:47:09 +0800

    代码提交检查

--- src/containers/home/app.js
+++ src/containers/home/app.js
@@ -5,7 +5,7 @@ import 'antd/dist/antd.css'
 const Battle = lazy(() => import('./battle.js'))
 const Popular = lazy(() => import('./popular'))
 
-const aa = 22
+const aa = 222
 function App() {
   return (
     <div className='App'>
